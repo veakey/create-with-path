cd8eb38a39f01bfc6328bccb94da1e09810538ff
Allen Kim <allen.kim@rci.rogers.ca>
2018-06-14 16:31:58 -0400

    updated

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
+const path = require('path'),
+  mustache = require('mustache'),
+  fs = require('fs-extra');
+  inquirer = require('inquirer');
 
-const packageName = process.argv[2];
-if (!packageName) {
-  console.error('Please enter the package name you want to create');
-  process.exit(1);
-}
-
-function outputFileSync (file, ...args) {
-  const dir = path.dirname(file)
-  if (!fs.existsSync(dir)) {
-    dir.split(path.sep).reduce((currentPath, folder) => {
-      currentPath += folder + path.sep;
-      if (!fs.existsSync(currentPath)) {
-        fs.mkdirSync(currentPath);
-      }
-      return currentPath;
-    }, '');
-  }
-  fs.writeFileSync(file, ...args)
-}
-
-function walkDir(dir, runIt) {
+function walkDir(dir, callback) {
   fs.readdirSync(dir).forEach( f => {
-    if (fs.statSync(path.join(dir, f)).isDirectory()) {
-      walkDir(path.join(dir, f), runIt);
-    } else {
-      runIt(path.join(dir, f));
-    }
+    let dirPath = path.join(dir, f);
+    let isDirectory = fs.statSync(dirPath).isDirectory();
+    isDirectory ? 
+      walkDir(dirPath, callback) : callback(path.join(dir, f));
   });
 };
 
-//write files from template directory
-walkDir(path.resolve(path.join(__dirname, 'template')), function(filePath) {
+function toQuestions(npmInit) {
+  const questions = [];
+  for (var key in npmInit.prompts) {
+    let question = npmInit.prompts[key];
+    question.name = key;
+    if (question.required) {
+      question.validate = val => Promise.resolve(!!val);
+    }
+    if (question.type === 'string') {
+      question.type = 'input';
+      question.filter = val => Promise.resolve(val.toLowerCase().replace(/\s+/g, '-'));   
+    }
+    questions.push(question);
+  }
+  return questions;
+}
+
+function copyTemplate(params) {
   const cwd = process.cwd();
-  const newPath = filePath.replace(new RegExp('.*template'+path.sep),'');
-  const newFilePath = path.join(cwd, packageName, newPath);
-  const contents = fs.readFileSync(filePath, 'utf8');
-  outputFileSync(newFilePath, contents); 
-});
+  const filePath = params.filePath.replace(params.templateDir,'');
+  const targetDir = path.join(cwd, filePath);
+  const fileContents = fs.readFileSync(params.filePath, 'utf8');
+  const pkgName = params.answers.name;
+
+  const contents = mustache.render(fileContents, params.answers);
+  const outputPath = path.join(cwd, pkgName, filePath);
+  console.log({outputPath, contents})
+  fs.outputFileSync(outputPath, contents);
+}
+
+function npmInitBase(npmInit, templateDir) {
+  const questions = toQuestions(npmInit);
+  inquirer.prompt(questions).then(answers => {
+    walkDir(templateDir, filePath => {
+      copyTemplate({templateDir, filePath, answers})
+    });
+    return answers;
+  }).then(answers => {
+    console.log(mustache.render(npmInit.completeMessage, answers));
+  }).catch(e => console.error(e) && process.exit(1));
+}
+
+const npmInit = require('./npm-init.json');
+const templateDir = require('path').resolve('./template');
+
+npmInitBase(npmInit, templateDir);
